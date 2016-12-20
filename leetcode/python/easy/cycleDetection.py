2f791f1883165cc3910a478bdb6246f872ea5a84
varshajoshi36 <varshajoshi36@gmail.com>
2016-12-19 22:38:03 -0800

    LinkedList problems

--- leetcode/python/easy/cycleDetection.py
+++ leetcode/python/easy/cycleDetection.py
@@ -1,5 +1,11 @@
 import ListNode from ../medium/ListNode
 
+'''
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?'''
+
 class DetectCycle(object):
 	def hasCycle(head):
 		if head is None:
---
+++ leetcode/python/easy/deleteNode.py
@@ -0,0 +1,19 @@
+import ListNode from ../medium/ListNode
+
+'''
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+'''
+
+
+class DeleteNode(object):
+	def deleteNode(node):
+		while node.next is not None:
+			while node.next is not None:
+                        	if node.next.next is None:
+                                	node.val = node.next.val
+	                                node.next = None
+        	                else:
+                	                node.val = node.next.val
+                        	        node = node.next
