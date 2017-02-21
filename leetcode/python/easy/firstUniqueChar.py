71d934184fb9953335e026afae75c8b9f9b1a443
varshajoshi36 <varshajoshi36@gmail.com>
2017-02-20 21:04:12 -0800

    reverse words

---
+++ leetcode/python/easy/firstUniqueChar.py
@@ -0,0 +1,25 @@
+'''
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+'''
+
+def firstUniqChar(s):
+	return_index = -1
+	for index in range(len(s)):
+		ch = s[index]
+		if ch not in s[index + 1:] and ch not in s[:index]:
+			return_index = index
+			break
+	return return_index
+
+
+s = 'loveleetcode'
+print firstUniqChar(s)
