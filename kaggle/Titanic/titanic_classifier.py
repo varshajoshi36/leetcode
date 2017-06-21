aff47a2a6fd72d94e274e45223819c326547d8dd
varshajoshi36 <varshajoshi36@gmail.com>
2017-06-21 16:45:09 -0700

    corrections

--- kaggle/Titanic/titanic_classifier.py
+++ kaggle/Titanic/titanic_classifier.py
@@ -7,14 +7,9 @@ def get_model(train_x, train_y):
     classifier = clf.fit(train_x, train_y)
     return classifier
 
-def create_data():
-    train_data = read_csv('train.csv', sep=',')
-    test_data = read_csv('test.csv', sep=',')
-
-    print test_data.shape
-    #train_data = train_data.dropna()
-    #test_data = test_data.dropna()
-    print test_data.shape
+def predict():
+    train_data = read_csv('data/train.csv', sep=',')
+    test_data = read_csv('data/test.csv', sep=',')
 
     train_y = train_data['Survived']
 
@@ -24,24 +19,19 @@ def create_data():
     train_x = pandas.concat([train_class, train_data.iloc[:, 4:8]], axis=1)    
     test_x = pandas.concat([test_class, test_data.iloc[:, 3:7]], axis=1)    
     
-    print test_x.shape
     model = get_model(train_x, train_y)
     predictions = model.predict(test_x)
-    print type(predictions)
 
     pass_id = test_data.iloc[:, 0]
     pred_df = pandas.DataFrame(predictions)
-    #print pass_id.shape
-    #print len(predictions)
-    #out_data = pandas.Panel({'PassengerId': pass_id, 'Survived': predictions})
     pass_id.reset_index(drop=True, inplace=True)
     pred_df.reset_index(drop=True, inplace=True)
     out_data = pandas.concat([pass_id, pred_df], axis=1, keys=["PassengerId", "Survived"])
-    out_data.to_csv('out.csv', sep=',')
+    out_data.to_csv('data/out.csv', sep=',')
 
 
 def main():
-    create_data()
+    predict()
 
 if __name__ == '__main__':
     main()
